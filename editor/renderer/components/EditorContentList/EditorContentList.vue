e6c2bedc3544ee69cd469e8dd63dbeb043b8e4b1
Egor Skleynov <e.skleynov@adictgroup.ru>
2026-04-27 23:16:35 +0800

    Исправляет рендер checklist-списков

--- editor/renderer/components/EditorContentList/EditorContentList.vue
+++ editor/renderer/components/EditorContentList/EditorContentList.vue
@@ -14,23 +14,51 @@ defineProps<{
 function getListTag(style: ListBlockData['style']): string {
   return style === 'ordered' ? 'ol' : 'ul'
 }
+
+function isChecklist(style: ListBlockData['style']): boolean {
+  return style === 'checklist'
+}
+
+function isChecklistItemChecked(item: ListBlockItem): boolean {
+  return item.meta?.checked === true
+}
 </script>
 
 <template>
   <component
     :is="getListTag(style)"
-    :class="$style.list"
+    :class="[$style.list, isChecklist(style) ? $style.listChecklist : '']"
   >
     <li
       v-for="(item, index) in items"
       :key="`${item.content}-${index}`"
+      :class="[$style.item, isChecklist(style) ? $style.checklistItem : '']"
     >
-      <span v-html="sanitizeInlineHtml(item.content)" />
-      <EditorContentList
-        v-if="item.items.length > 0"
-        :items="item.items"
-        :style="style"
-      />
+      <template v-if="isChecklist(style)">
+        <input
+          :checked="isChecklistItemChecked(item)"
+          :class="$style.checkbox"
+          disabled
+          tabindex="-1"
+          type="checkbox"
+        />
+        <div :class="$style.content">
+          <span v-html="sanitizeInlineHtml(item.content)" />
+          <EditorContentList
+            v-if="item.items.length > 0"
+            :items="item.items"
+            :style="style"
+          />
+        </div>
+      </template>
+      <template v-else>
+        <span v-html="sanitizeInlineHtml(item.content)" />
+        <EditorContentList
+          v-if="item.items.length > 0"
+          :items="item.items"
+          :style="style"
+        />
+      </template>
     </li>
   </component>
 </template>
