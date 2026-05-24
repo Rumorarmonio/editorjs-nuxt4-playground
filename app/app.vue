065d9f6b06fd7dcf52e1414993d9ef718b3c58f3
Egor Skleynov <e.skleynov@adictgroup.ru>
2026-05-24 13:51:46 +0800

    Добавляет title и favicon для приложения

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { computed, onMounted } from 'vue'
+import { useI18n } from 'vue-i18n'
 import { installToastDebugApi } from '~~/shared/notifications'
+
+const route = useRoute()
+const { t } = useI18n()
 
 useAppLocale()
 useAppTheme()
+
+const pageTitle = computed(() =>
+  route.path.startsWith('/preview')
+    ? t('app.previewPage.title')
+    : t('app.editorPage.title'),
+)
+
+useHead(() => ({
+  link: [
+    {
+      href: '/favicon.svg',
+      rel: 'icon',
+      type: 'image/svg+xml',
+    },
+  ],
+  title: pageTitle.value,
+}))
 
 onMounted(() => {
   installToastDebugApi()
